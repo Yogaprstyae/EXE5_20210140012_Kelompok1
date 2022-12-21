22e1022a4659388c1e29cede3064b2e24675a426
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-12-21 15:38:10 +0700

    Add project files.

---
+++ EXE5_20210140012_Kelompok1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20210140012_Kelompok1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
